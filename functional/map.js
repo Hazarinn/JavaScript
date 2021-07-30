b10b4960e091de8ac3fd499d3daa0db2302c090a
Rubens Diego <devbsb.rubens@gmail.com>
2021-07-30 17:40:55 -0300

    estudos

---
+++ functional/map.js
@@ -0,0 +1,24 @@
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const numbersV2 = numbers.map((el) => {
+    return el * 2
+
+})
+
+
+
+console.log(numbers, numbersV2)
+
+const students = [
+
+    { name: 'jake', score: 6.4 },
+    { name: 'susan', score: 8.6 },
+    { name: 'emma', score: 9.4 },
+    { name: 'peter', score: 9.1},
+]
+
+const getScore = el => el.score
+
+
+
+console.log(students.map(getScore).map(Math.ceil))
